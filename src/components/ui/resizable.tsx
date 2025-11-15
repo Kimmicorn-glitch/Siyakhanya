23ac07ee417f8dadcacfd9a848aa722cc46936c6
Kimberley Bezuidenhout <kimberleybezuidenhout@gmail.com>
2025-11-15 08:28:06 +0000

    Try fixing this error: `Build Error: Export PanelHandle doesn't exist in

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,10 +1,14 @@
 "use client"
 
-import { PanelGroup as PanelGroupPrimitive, type PanelGroupProps } from "react-resizable-panels"
+import {
+  PanelGroup as PanelGroupPrimitive,
+  type PanelGroupProps,
+  Panel,
+  PanelResizeHandle,
+  type PanelResizeHandleProps,
+} from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
-import { Panel, PanelHandle, PanelResizeHandleProps } from "react-resizable-panels"
-
 
 const ResizablePanelGroup = ({
   className,
@@ -26,7 +30,7 @@ const ResizableHandle = ({
   className,
   ...props
 }: PanelResizeHandleProps & { withHandle?: boolean }) => (
-  <PanelHandle
+  <PanelResizeHandle
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
@@ -38,7 +42,7 @@ const ResizableHandle = ({
         <div className="h-2 w-px bg-muted-foreground" />
       </div>
     )}
-  </PanelHandle>
+  </PanelResizeHandle>
 )
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
